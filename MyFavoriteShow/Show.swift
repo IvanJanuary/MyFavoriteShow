08703897bbbdf7cc904b3a485cd46b1cbc6da6c8
Ivan <>
2022-01-20 14:59:12 +0200

    Added the structure Show

---
+++ MyFavoriteShow/Show.swift
@@ -0,0 +1,14 @@
+//
+//  Show.swift
+//  MyFavoriteShow
+//
+//  Created by Ivan on 18.01.2022.
+//
+
+import Foundation
+
+struct Show {
+    let imageName: String
+    let name: String
+    let description: String
+}
